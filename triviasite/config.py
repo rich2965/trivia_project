2fd2fc36198b0de669e8ea16e9ff0b2a6dc3c80e
Rich <rich2965@gmail.com>
2023-03-28 17:18:42 +0000

    taking out user/account functions

--- triviasite/config.py
+++ triviasite/config.py
@@ -11,8 +11,3 @@ class Config:
             db_host=os.environ.get('db_host'),
             db_port=os.environ.get('db_port')
     )
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') 
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
