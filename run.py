d9e572e4d4179ceed0647073a43ba491e07ba8c1
Rich <rich2965@gmail.com>
2023-04-05 17:32:26 -0500

    improvements to movie page, removed unused files

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-from triviasite import create_app
-
-app = create_app()
+from triviasite import app
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=True)
--- triviasite/__init__.py
+++ triviasite/__init__.py
@@ -1,33 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-from flask_mail import Mail
 from triviasite.config import Config
 
 
 db = SQLAlchemy()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
-login_manager.login_view ='login'
-login_manager.login_message_category = 'info'
-mail = Mail()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
-bcrypt.init_app(app)
-login_manager.init_app(app)
-mail.init_app(app)
 
-from triviasite.users.routes import users
-from triviasite.questions.routes import questions
 from triviasite.main.routes import main
 from triviasite.errors.handlers import errors
 
-app.register_blueprint(users)
-app.register_blueprint(questions)
 app.register_blueprint(main)
 app.register_blueprint(errors)
 
